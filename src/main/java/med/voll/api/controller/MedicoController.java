c24754db17080ac3d4953e1c4a9f2b4c636461a7
Churros <klebersonluiz5@gmail.com>
2023-05-12 23:27:19 -0300

    Validação com Bean Validation + Nova migration no banco de dados

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,5 +1,6 @@
 package med.voll.api.controller;
 
+import jakarta.validation.Valid;
 import med.voll.api.medico.DadosCadastroMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
@@ -19,7 +20,7 @@ public class MedicoController {
 
     @PostMapping
     @Transactional
-    public void cadastrar(@RequestBody DadosCadastroMedico dados) { //requestbody e para o json pegar os valores do corpo
+    public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados) { //requestbody e para o json pegar os valores do corpo
     repository.save(new Medico(dados));
     }
 

16d07b4b25fbea7cfd5e173a59cfc7cf0052e24d
Churros <klebersonluiz5@gmail.com>
2023-05-22 20:39:42 -0300

    Padronizando o código HTTP 201

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,31 +21,33 @@ public class MedicoController {
 
     @PostMapping
     @Transactional
-    public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados) { //requestbody e para o json pegar os valores do corpo
+    public ResponseEntity cadastrar(@RequestBody @Valid DadosCadastroMedico dados) { //requestbody e para o json pegar os valores do corpo
     repository.save(new Medico(dados));
     }
     @GetMapping
-    public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao){
+    public ResponseEntity<Page<DadosListagemMedico>> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao){
         // page devolve a lista e a paginação
-     return repository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new);
+     var page =  repository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new);
      // convertendo de medico, para dados listagem medico
-
+        return ResponseEntity.ok(page);
     }
     @PutMapping
     @Transactional
-    public void atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados){
+    public ResponseEntity atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados){
         var medico = repository.getReferenceById(dados.id());
         medico.atualizarInformacoes(dados);
 
+        return ResponseEntity.ok(new DadosDetalhamentoMedico(medico));
     }
     @DeleteMapping("/{id}") // o Spring sabe que o /id e um parametro da anotacao deletemapping
     @Transactional
-    public void excluir(@PathVariable Long id){
+    public ResponseEntity excluir(@PathVariable Long id){
         // é uma varivel do @PathVariable do caminho url
         // mostra que parametro Long id e equivalente do valor /{id}
 
         var medico = repository.getReferenceById(id);
         medico.excluir();
+        return ResponseEntity.noContent().build();
 
     }
 

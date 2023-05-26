5242a80478fcf05b5f1a5719365525046e1d9da0
Churros <klebersonluiz5@gmail.com>
2023-05-26 20:47:23 -0300

    tratando erro 400

--- src/main/java/med/voll/api/infra/TratadorDeErros.java
+++ src/main/java/med/voll/api/infra/TratadorDeErros.java
@@ -2,6 +2,9 @@ package med.voll.api.infra;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.support.MetaDataAccessException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -10,6 +13,20 @@ public class TratadorDeErros {
     @ExceptionHandler(EntityNotFoundException.class) // em qualquer controller for lançado uma exception EntityNotFoundException ele sabe que e para chamar esse metodo 404
     public ResponseEntity tratarErro404(){
     return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class) //
+    public ResponseEntity tratarErro400(MethodArgumentNotValidException ex){
+        var erros = ex.getFieldErrors(); // ele vai devolver uma lista contendo cada um dos erros dos campos
+
+        return ResponseEntity.badRequest().body(erros.stream().map(DadosErroValidacao::new).toList()); // body ja devolve o obj ResponseEntity body(erros) ele vai devolver o codigo 400 e a lista no corpo
+    }
+
+    private record DadosErroValidacao(String campo, String mensagem){
+    public DadosErroValidacao(FieldError erro){
+        this(erro.getField(), erro.getDefaultMessage());
+        // que da o nome do campo e da a mensagem daquele campo especifico
+    }
     }
 
 }
